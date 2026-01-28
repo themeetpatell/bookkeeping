76363ff1907f8d30c292d87f34aa660dc44626e4
Meet Patel <themeetpatel@Meets-MacBook-Pro.local>
2026-01-28 14:28:38 +0400

    updatedf colors

--- src/pages/AccountingLanding.jsx
+++ src/pages/AccountingLanding.jsx
@@ -23,28 +23,30 @@ const ZohoConsultationForm = ({ formId }) => (
     <input type="hidden" name="gclid" value="" />
     <input type="hidden" name="referrername" value="" />
     
-    <div className="form-group">
-      <label>First Name</label>
-      <input
-        type="text"
-        maxLength="255"
-        name="Name_First"
-        fieldType="7"
-        placeholder="John"
-        className="form-control"
-      />
-    </div>
-    
-    <div className="form-group">
-      <label>Last Name</label>
-      <input
-        type="text"
-        maxLength="255"
-        name="Name_Last"
-        fieldType="7"
-        placeholder="Smith"
-        className="form-control"
-      />
+    <div className="form-row">
+      <div className="form-group">
+        <label>First Name</label>
+        <input
+          type="text"
+          maxLength="255"
+          name="Name_First"
+          fieldType="7"
+          placeholder="John"
+          className="form-control"
+        />
+      </div>
+      
+      <div className="form-group">
+        <label>Last Name</label>
+        <input
+          type="text"
+          maxLength="255"
+          name="Name_Last"
+          fieldType="7"
+          placeholder="Smith"
+          className="form-control"
+        />
+      </div>
     </div>
     
     <div className="form-group">
